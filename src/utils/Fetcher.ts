ce5832c4fad007c226d94237e066420e1446b465
Agustina Aldasoro <agusaldasoro@gmail.com>
2020-12-09 09:36:50 -0300

    Refactor in Fetcher to support headers on every method

--- src/utils/Fetcher.ts
+++ src/utils/Fetcher.ts
@@ -1,96 +1,147 @@
-import ms from "ms";
-import AbortController from 'abort-controller';
+import ms from 'ms'
+import AbortController from 'abort-controller'
 import crossFetch from 'cross-fetch'
 import blobToBuffer from 'blob-to-buffer'
-import { clearTimeout, setTimeout } from "timers"
-import { applyDefaults, retry } from "./Helper";
+import { clearTimeout, setTimeout } from 'timers'
+import { retry } from './Helper'
 
 export class Fetcher {
+  private readonly defaults = {
+    method: 'GET',
+    responseType: ResponseType.JSON,
+    attempts: 1,
+    waitTime: '1s',
+    timeout: '5m'
+  }
 
-    constructor(private readonly defaultJsonRequestTimeout: string = '30s',
-        private readonly defaultFileDownloadRequestTimeout: string = '1m',
-        private readonly defaultPostTimeout: string = '5m') {}
-
-    async fetchJson(url: string, options?: RequestOptions): Promise<any> {
-        const opts = applyDefaults({ attempts: 1,
-            timeout: this.defaultJsonRequestTimeout,
-            waitTime: '0.5s',
-        }, options)
-        return this.fetchInternal(url, response => response.json(), opts)
+  private readonly fetchJsonDefaults = {
+    ...this.defaults,
+    ...{
+      timeout: '30s',
+      waitTime: '0.5s'
     }
-
-    async fetchBuffer(url: string, options?: RequestOptions): Promise<Buffer> {
-        const opts = applyDefaults({ attempts: 1,
-            timeout: this.defaultFileDownloadRequestTimeout,
-            waitTime: '1s',
-        }, options)
-        return this.fetchInternal(url, response => this.extractBuffer(response), opts)
+  }
+  private readonly fetchBufferDefaults = {
+    ...this.defaults,
+    ...{
+      timeout: '1m',
+      responseType: ResponseType.Buffer
     }
-
-    async postForm(url: string, body: FormData | string, headers: any = { }, options?: RequestOptions): Promise<any> {
-        const opts = applyDefaults({ attempts: 1,
-            timeout: this.defaultPostTimeout,
-            waitTime: '1s',
-        }, options)
-
-        return this.fetchInternal(url, response => response.json(), opts, 'POST', body, headers)
+  }
+  private readonly postDefaults = {
+    ...this.defaults,
+    ...{
+      method: 'POST'
     }
+  }
 
-    private async fetchInternal<T>(url: string, responseConsumer: (response: Response) => Promise<T>, options: CompleteRequestOptions, method: string = 'GET', body?: FormData | string, headers: any = { }): Promise<T> {
-        return retry(async () => {
-            const controller = new AbortController();
-            const timeout = setTimeout(() => {
-                controller.abort();
-            }, ms(options.timeout));
+  constructor() {}
 
-            try {
-                const response = await crossFetch(url, { signal: controller.signal, body, method, headers });
-                if (response.ok) {
-                    return await responseConsumer(response)
-                } else {
-                    const responseText = await response.text()
-                    throw new Error(`Failed to fetch ${url}. Got status ${response.status}. Response was '${responseText}'`)
-                }
-            } finally {
-                clearTimeout(timeout)
-            }
-        }, options.attempts, options.waitTime)
+  async fetchJson(options: Partial<RequestOptions> & Pick<RequestOptions, 'url'>): Promise<any> {
+    return this.fetchInternal(this.responseJsonHandler(), Object.assign(this.fetchJsonDefaults, options))
+  }
+
+  async fetchBuffer(options: Partial<RequestOptions> & Pick<RequestOptions, 'url'>): Promise<Buffer> {
+    return this.fetchInternal(this.responseBufferHandler(), Object.assign(this.fetchBufferDefaults, options))
+  }
+
+  async postForm(options: Partial<RequestOptions> & Pick<RequestOptions, 'url'>): Promise<any> {
+    return this.fetchInternal(this.responseJsonHandler(), Object.assign(this.postDefaults, options))
+  }
+
+  async queryGraph<T = any>(
+    query: string,
+    variables: Record<string, any>,
+    options: Partial<RequestOptions> & Pick<RequestOptions, 'url'>
+  ): Promise<T> {
+    const response = await this.postForm(
+      Object.assign(
+        { body: JSON.stringify({ query, variables }), headers: { 'Content-Type': 'application/json' } },
+        options
+      )
+    )
+    if (response.errors) {
+      throw new Error(`Error querying graph. Reasons: ${JSON.stringify(response.errors)}`)
     }
+    return response.data
+  }
 
-    async queryGraph<T = any>(url: string, query: string, variables: Record<string, any>, options?: RequestOptions): Promise<T> {
-        const json = await this.postForm(url, JSON.stringify({ query, variables }), { 'Content-Type': 'application/json' }, options)
-        if (json.errors) {
-            throw new Error(
-                `Error querying graph. Reasons: ${JSON.stringify(json.errors)}`
-            )
-        }
-        return json.data
-    }
+  private async fetchInternal<T>(
+    responseConsumer: (response: Response) => Promise<T>,
+    options: RequestOptions
+  ): Promise<T> {
+    return retry(
+      async () => {
+        const controller = new AbortController()
+        const timeout = setTimeout(() => {
+          controller.abort()
+        }, ms(options.timeout))
 
-    private async extractBuffer(response: Response): Promise<Buffer> {
-        if ('buffer' in response) {
-            // @ts-ignore
-            return response.buffer()
-        }
-        const blob = await response.blob()
-        return this.asyncBlobToBuffer(blob)
-    }
-
-    private asyncBlobToBuffer(blob: Blob): Promise<Buffer> {
-        return new Promise((resolve, reject) => {
-          blobToBuffer(blob, (err: Error, buffer: Buffer) => {
-            if (err) reject(err)
-            resolve(buffer)
+        try {
+          const response = await crossFetch(options.url, {
+            signal: controller.signal,
+            body: options.body,
+            method: options.method,
+            headers: options.headers
           })
-        })
+          if (response.ok) {
+            return await responseConsumer(response)
+          } else {
+            const responseText = await response.text()
+            throw new Error(
+              `Failed to fetch ${options.url}. Got status ${response.status}. Response was '${responseText}'`
+            )
+          }
+        } finally {
+          clearTimeout(timeout)
+        }
+      },
+      options.attempts,
+      options.waitTime
+    )
+  }
+
+  private responseJsonHandler(): (response: Response) => Promise<any> {
+    return (response) => response.json()
+  }
+
+  private responseBufferHandler(): (response: Response) => Promise<any> {
+    return (response) => this.extractBuffer(response)
+  }
+
+  private async extractBuffer(response: Response): Promise<Buffer> {
+    if ('buffer' in response) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      return response.buffer()
     }
+    const blob = await response.blob()
+    return this.asyncBlobToBuffer(blob)
+  }
+
+  private asyncBlobToBuffer(blob: Blob): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      blobToBuffer(blob, (err: Error, buffer: Buffer) => {
+        if (err) reject(err)
+        resolve(buffer)
+      })
+    })
+  }
 }
 
-export type RequestOptions = Partial<CompleteRequestOptions>
+enum ResponseType {
+  JSON,
+  Buffer
+}
 
-type CompleteRequestOptions = {
-    attempts: number, // Number of attempts to perform the request
-    timeout: string, // Time to abort the request. Time format accepted by ms
-    waitTime: string, // Time to wait between attempts. Time format accepted by ms
-    // Time format accepted by ms: Examples: '0.5s', '2m', '3h', '100' (assumed to be milliseconds)
+type RequestOptions = {
+  url: string
+  method: string
+  responseType: ResponseType
+  // Time format accepted by ms: Examples: '0.5s', '2m', '3h', '100' (assumed to be milliseconds)
+  attempts: number // Number of attempts to perform the request
+  timeout: string // Time to abort the request. Time format accepted by ms
+  waitTime: string // Time to wait between attempts. Time format accepted by ms
+  body?: FormData | string
+  headers?: Record<string, string>
 }
--- test/utils/Fetcher.spec.ts
+++ test/utils/Fetcher.spec.ts
@@ -1,21 +1,77 @@
 import chai from 'chai'
+import mockttp = require('mockttp')
 import chaiAsPromised from 'chai-as-promised'
-import { Fetcher } from 'utils'
+import { delay, Fetcher } from 'utils'
 
 chai.use(chaiAsPromised)
 const expect = chai.expect
+const mockServer = mockttp.getLocal()
 
 describe('Fetcher', () => {
+  beforeEach(() => {
+    mockServer.start(8080)
+  })
+  afterEach(() => mockServer.stop())
 
-    it('When a timeout is set, then the fetch is aborted', async () => {
-        const fetcher = new Fetcher()
-        const fetch = fetcher.fetchBuffer('https://httpstat.us/200?sleep=10000', { timeout: '5s' })
-        await expect(fetch).to.be.rejectedWith('The user aborted a request.')
-    }).timeout('15s')
+  it('When a timeout is set, then the fetch is aborted', async () => {
+    mockServer.get('/mocked-path').thenCallback(async () => {
+      await delay('10000')
+      return {}
+    })
 
-    it('When a timeout is set but not reached, then the fetch is successful', async () => {
-        const fetcher = new Fetcher()
-        await fetcher.fetchBuffer('https://httpstat.us/200', { timeout: '5s' })
-    }).timeout('10s')
+    const fetch = new Fetcher().fetchBuffer({ url: 'http://localhost:8080/mocked-path', timeout: '5s' })
 
+    await expect(fetch).to.be.rejectedWith('The user aborted a request.')
+  }).timeout('15s')
+
+  it('When a timeout is set but not reached, then the fetch is successful', async () => {
+    await mockServer.get('/mocked-path').thenReply(200, '{"body": "matching body"}')
+
+    const fetch = new Fetcher().fetchBuffer({ url: 'http://localhost:8080/mocked-path', timeout: '5s' })
+
+    expect((await fetch).toString()).to.include('matching body')
+  }).timeout('10s')
+
+  it('When making a POST with custom headers they are sent to the upstream', async () => {
+    await mockServer.post('/mocked-path').thenReply(200, '{"body": "matching body"}')
+
+    const fetch = new Fetcher().postForm({
+      url: 'http://localhost:8080/mocked-path',
+      headers: { 'user-agent': 'ContentServer/v2' }
+    })
+
+    expect((await fetch).body).to.include('matching body')
+    await assertFetchHasHeader('user-agent', 'ContentServer/v2')
+  }).timeout('10s')
+
+  it('When making a GET JSON with custom headers they are sent to the upstream', async () => {
+    await mockServer.get('/mocked-path').thenReply(200, '{"body": "matching body"}')
+
+    const fetch = new Fetcher().fetchJson({
+      url: 'http://localhost:8080/mocked-path',
+      headers: { 'user-agent': 'ContentServer/v2' }
+    })
+
+    expect((await fetch).body).to.include('matching body')
+    await assertFetchHasHeader('user-agent', 'ContentServer/v2')
+  }).timeout('10s')
+
+  it('When making a GET Buffer with custom headers they are sent to the upstream', async () => {
+    await mockServer.get('/mocked-path').thenReply(200, '{"body": "matching body"}')
+
+    const fetch = new Fetcher().fetchBuffer({
+      url: 'http://localhost:8080/mocked-path',
+      headers: { 'user-agent': 'ContentServer/v2' }
+    })
+
+    expect((await fetch).toString()).to.include('matching body')
+    await assertFetchHasHeader('user-agent', 'ContentServer/v2')
+  }).timeout('10s')
 })
+
+async function assertFetchHasHeader(headerKey: string, headerValue: string) {
+  const mockedEndpoints = await mockServer.getMockedEndpoints()
+  const seenRequests = await mockedEndpoints[0].getSeenRequests()
+  expect(seenRequests.length).to.equal(1)
+  expect(seenRequests[0].headers[headerKey]).to.equal(headerValue)
+}
