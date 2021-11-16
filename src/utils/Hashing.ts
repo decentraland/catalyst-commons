adb14415c0e564707115c9935bc1f2976623c055
menduz <github@menduz.com>
2021-11-16 16:29:17 -0300

    feat: add calculateMultipleHashesADR32 (#127)
    * feat: add calculateMultipleHashesADR32
    
    * chore: reuse types present in this project
    
    * chore: fix comment
    
    * add legacy hashes for compatibility

--- src/utils/Hashing.ts
+++ src/utils/Hashing.ts
@@ -1,16 +1,18 @@
 import CID from 'cids'
 import multihashing from 'multihashing-async'
 import ipfsHashing from 'ipfs-only-hash'
-import { ContentFileHash } from '../types'
+import { ContentFileHash, EntityContentItemReference, EntityMetadata } from '../types'
 
-export class Hashing {
+export namespace Hashing {
   /**
    * Given a set of files, return a map with their hash
    * @deprecated use calculateIPFSHashes instead
    */
-  static async calculateHashes<T extends Uint8Array>(files: T[]): Promise<{ hash: ContentFileHash; file: T }[]> {
+  export async function calculateHashes<T extends Uint8Array>(
+    files: T[]
+  ): Promise<{ hash: ContentFileHash; file: T }[]> {
     const entries = Array.from(files).map(async (file) => ({
-      hash: await this.calculateBufferHash(file),
+      hash: await calculateBufferHash(file),
       file
     }))
     return Promise.all(entries)
@@ -20,21 +22,74 @@ export class Hashing {
    * Return the given buffer's hash
    * @deprecated use calculateIPFSHash instead
    */
-  static async calculateBufferHash(buffer: Uint8Array): Promise<ContentFileHash> {
+  export async function calculateBufferHash(buffer: Uint8Array): Promise<ContentFileHash> {
     const hash = await multihashing(buffer, 'sha2-256')
     return new CID(0, 'dag-pb', hash).toBaseEncodedString()
   }
 
-  static async calculateIPFSHash(buffer: Uint8Array): Promise<ContentFileHash> {
+  export async function calculateIPFSHash(buffer: Uint8Array): Promise<ContentFileHash> {
     // CIDv1 requires rawLeaves: true
     return ipfsHashing.of(buffer, { cidVersion: 1, onlyHash: true, rawLeaves: true })
   }
 
-  static async calculateIPFSHashes<T extends Uint8Array>(files: T[]): Promise<{ hash: ContentFileHash; file: T }[]> {
+  export async function calculateIPFSHashes<T extends Uint8Array>(
+    files: T[]
+  ): Promise<{ hash: ContentFileHash; file: T }[]> {
     const entries = Array.from(files).map(async (file) => ({
-      hash: await this.calculateIPFSHash(file),
+      hash: await calculateIPFSHash(file),
       file
     }))
     return Promise.all(entries)
+  }
+
+  function prepareADR32Data(contents: EntityContentItemReference[], metadata?: EntityMetadata) {
+    // Compare both by key and hash
+    const sorter = (a: EntityContentItemReference, b: EntityContentItemReference) => {
+      if (a.hash > b.hash) return 1
+      else if (a.hash < b.hash) return -1
+      else return a.file > b.file ? 1 : -1
+    }
+
+    return new TextEncoder().encode(
+      JSON.stringify({
+        content: contents.sort(sorter).map((entry) => ({ key: entry.file, hash: entry.hash })),
+        metadata
+      })
+    )
+  }
+
+  /**
+   * Calculates the content hash of multiple files to be used consistently by the builder
+   * and other content-based applications when hashes need to be stored on-chain.
+   *
+   * Returns the CIDv1 of the data prepared to sign
+   */
+  export async function calculateMultipleHashesADR32(
+    contents: EntityContentItemReference[],
+    metadata?: EntityMetadata
+  ) {
+    const data = prepareADR32Data(contents, metadata)
+
+    return {
+      data,
+      hash: await Hashing.calculateIPFSHash(data)
+    }
+  }
+
+  /**
+   * Calculates the content hash of multiple files to be used consistently by the builder
+   * and other content-based applications when hashes need to be stored on-chain.
+   * @deprecated this is maintained only for compatibility reasons with calculateBufferHash (Qm prefix)
+   */
+  export async function calculateMultipleHashesADR32LegacyQmHash(
+    contents: EntityContentItemReference[],
+    metadata?: EntityMetadata
+  ) {
+    const data = prepareADR32Data(contents, metadata)
+
+    return {
+      data,
+      hash: await Hashing.calculateBufferHash(data)
+    }
   }
 }
--- test/utils/Hashing.spec.ts
+++ test/utils/Hashing.spec.ts
@@ -3,6 +3,7 @@ import chaiAsPromised from 'chai-as-promised'
 import { Hashing } from '../../src/utils/Hashing'
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
+import { EntityContentItemReference } from 'types'
 
 chai.use(chaiAsPromised)
 const expect = chai.expect
@@ -22,5 +23,37 @@ describe('Hashing', () => {
         readFileSync(resolve(__dirname, '../files/QmSYpJEQLQc82USvtavzxEiBR57nyb5RdMzecBTR3Qg6qn'))
       )
     ).to.eq('QmSYpJEQLQc82USvtavzxEiBR57nyb5RdMzecBTR3Qg6qn')
+  })
+
+  it('v1 hashes multiple files correctly 1', async () => {
+    const files: EntityContentItemReference[] = [
+      { file: 'a.png', hash: 'bafybeibdik2ihfpcdi7aaaguptwcoc5msav7uhn5hu54xlq2pdwkh5arzy' }
+    ]
+    const { hash } = await Hashing.calculateMultipleHashesADR32(files)
+    expect(hash).to.eq('bafkreigwbjbqaaf63q2cnbrqebctyo3a5y6oxos47usvexhvzajkoczspa')
+  })
+  it('v1 hashes multiple files correctly 2', async () => {
+    const files: EntityContentItemReference[] = [
+      { file: 'a.png', hash: 'bafybeibdik2ihfpcdi7aaaguptwcoc5msav7uhn5hu54xlq2pdwkh5arzy' },
+      { file: 'a/b.png', hash: 'bafybeibdik2ihfpcdi7aaaguptwcoc5msav7uhn5hu54xlq2pdwkh5asd' }
+    ]
+    const { hash } = await Hashing.calculateMultipleHashesADR32(files)
+    expect(hash).to.eq('bafkreih5bj5fxz72bgvhlqq35teesr75wysn2qcjayyi7kehdcyeiosgdi')
+  })
+  it('v1 hashes multiple files correctly, changing metadata changes the final hash', async () => {
+    const files: EntityContentItemReference[] = [
+      { file: 'a.png', hash: 'bafybeibdik2ihfpcdi7aaaguptwcoc5msav7uhn5hu54xlq2pdwkh5arzy' },
+      { file: 'a/b.png', hash: 'bafybeibdik2ihfpcdi7aaaguptwcoc5msav7uhn5hu54xlq2pdwkh5asd' }
+    ]
+    const { hash } = await Hashing.calculateMultipleHashesADR32(files, { key: 'value' })
+    expect(hash).to.eq('bafkreieusocjbdoxg5cdqtysltk353l3mtmzvtyhza6zxvwfjsfjrcm2ze')
+  })
+  it('v0 hashes multiple files correctly, changing metadata changes the final hash', async () => {
+    const files: EntityContentItemReference[] = [
+      { file: 'a.png', hash: 'bafybeibdik2ihfpcdi7aaaguptwcoc5msav7uhn5hu54xlq2pdwkh5arzy' },
+      { file: 'a/b.png', hash: 'bafybeibdik2ihfpcdi7aaaguptwcoc5msav7uhn5hu54xlq2pdwkh5asd' }
+    ]
+    const { hash } = await Hashing.calculateMultipleHashesADR32LegacyQmHash(files, { key: 'value' })
+    expect(hash).to.eq('QmYLdWnSPor5Ycr6MdaaqwsbTLsnvRtw8MPZ5oitMKoztg')
   })
 })
